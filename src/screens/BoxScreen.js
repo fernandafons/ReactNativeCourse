00c83f491d87a25c89f28f221728667c945358cb
fernandafons <nanda.fonseca7@gmail.com>
2021-01-14 22:43:57 -0300

    just added some more options in css styling into Box Screen

--- src/screens/BoxScreen.js
+++ src/screens/BoxScreen.js
@@ -4,7 +4,9 @@ import { View, Text, StyleSheet } from 'react-native';
 const BoxScreen = () => {
     return (
         <View style={styles.viewStyle}>
-            <Text style={styles.textStyle}>Box Screen</Text>
+            <Text style={styles.textOneStyle}>Child n1</Text>
+            <Text style={styles.textTwoStyle}>Child n2</Text>
+            <Text style={styles.textThreeStyle}>Child n3</Text>
         </View>
     );
 };
@@ -12,12 +14,31 @@ const BoxScreen = () => {
 const styles = StyleSheet.create({
     viewStyle: {
         borderWidth: 3,
-        borderColor: 'black'
+        borderColor: 'black',
+        height: 200,
+        flexDirection: 'row',
+        alignItems: 'baseline',
+        justifyContent: 'space-around'
     },
-    textStyle: {
+    textOneStyle: {
         borderWidth: 3,
         borderColor: 'red',
-        margin: 20
+        // flex: 1
+    },
+    textTwoStyle: {
+        borderWidth: 3,
+        borderColor: 'red',
+        // position: 'absolute',
+        top: 30
+        // flex: 1,
+        // alignSelf: 'flex-end',
+        // ...StyleSheet.absoluteFillObject // fullfill parent's space
+        // position: 'absolute' // override, ignore its siblings
+    },
+    textThreeStyle: {
+        borderWidth: 3,
+        borderColor: 'red',
+        // flex: 1
     }
 });
 
